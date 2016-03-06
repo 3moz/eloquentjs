6806cc253f13c98130c0dff2fc0e8643791ba1b7
Eric Moskowitz <ericmoskowitz@Erics-MacBook-Pro.local>
2016-03-06 15:34:17 -0500

    ch01ex03pt02

--- ch01/chess2.js
+++ ch01/chess2.js
@@ -1,20 +1,29 @@
-var size = 8;
-var str = '';
-var counter = 0
-for (var k = 0; k < size; k++){
-  for (var i = 0; i <= size; i++){
-    if (k%2===0){
-      if(i%2===0 && i!==size){
-        str+='#';
-      } else str+=' ';
-    } else if (k%2!==0){
-      if(i%2===0){
-        str+=' ';
-      } else str+='#';
-    }
-    if (i===size){
-      str+='\n';
+ //take number arg for grid dimensions from CLI
+var size = parseInt(process.argv[2]);
+
+function showall(size){
+  var str = '';
+  for (var k = 0; k < size; k++){
+    for (var i = 0; i <= size; i++){
+      if (k%2===0){
+        if(i%2===0 && i!==size){
+          str+='#';
+        } else str+=' ';
+      } else if (k%2!==0){
+        if(i%2===0){
+          str+=' ';
+        } else str+='#';
+      }
+      if (i===size){
+        str+='\n';
+      }
     }
   }
+  console.log(str);
 }
-console.log(str);
+
+//print a countdown of all chessboards of dimensions 'size' down to 0
+for (var n = size; n >= 0; n-- ){
+  showall(n);
+  size--;
+}
