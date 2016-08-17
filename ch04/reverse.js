d5534226803d10c13398a85a7d5d3b1f7fec858e
Eric Moskowitz <moskowitz.e@gmail.com>
2016-08-17 09:18:14 -0400

    reverse done

--- ch04/reverse.js
+++ ch04/reverse.js
@@ -3,4 +3,13 @@ var outputArr = []
 for (var k = 0; k < inputArr.length; k++){
   outputArr.push(parseInt(inputArr[k]));
 }
-console.info(outputArr, typeof outputArr);
+
+var newArr = [];
+var reverse = function() {
+  for (var i = outputArr.length-1; i >= 0; i--) {
+    newArr.push(outputArr[i])
+  }
+  return newArr;
+}
+
+console.log(reverse(outputArr));
