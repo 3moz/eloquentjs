ed310e674d13e48414d559bc77a52e6ab9350f54
Eric Moskowitz <moskowitz.e@gmail.com>
2017-05-24 17:30:06 -0400

    vectorType get

--- ch06/vectorType.js
+++ ch06/vectorType.js
@@ -11,6 +11,14 @@ Vector.prototype.minus = function(vec) {
     return new Vector(this.x-vec.x, this.y-vec.y)
 }
 
+Object.defineProperty(Vector.prototype, "length", {
+    get: function() {
+        return Math.sqrt(Math.pow(this.x,2)+Math.pow(this.y,2))
+    }
+})
+
+
+
 console.log(new Vector(1,2).plus(new Vector(2,3)));
 console.log(new Vector(1,2).minus(new Vector(2,3)));
 console.log(new Vector(3,4).length);
